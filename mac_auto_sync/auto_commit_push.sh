720c867f81f7b3995f188af39a533d8519f9c28c
Erik Suen <suenfung2005@gmail.com>
2020-03-12 22:35:48 +0800

    auto commit

--- mac_auto_sync/auto_commit_push.sh
+++ mac_auto_sync/auto_commit_push.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+BASEDIR=$(dirname "$0")
+cd $BASEDIR
+echo $(pwd)
 git add .
 git commit -m "auto commit" -a
 git push origin master
--- mac_auto_sync/setup.sh
+++ mac_auto_sync/setup.sh
@@ -11,4 +11,6 @@ chmod +x auto_commit_push.sh
 
 cp com.eriksuen.autosync_dotfiles.plist ~/Library/LaunchAgents
 
+sed -i "" "s;\$HOME;${HOME};g" ~/Library/LaunchAgents/com.eriksuen.autosync_dotfiles.plist
+
 launchctl load -w ~/Library/LaunchAgents/com.eriksuen.autosync_dotfiles.plist
