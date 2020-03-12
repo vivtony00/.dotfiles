fdbe53002d3551b75b9c04d84cfdffe136b085f8
Erik Suen <suenfung2005@gmail.com>
2020-03-12 16:20:45 +0800

    auto commit

---
+++ mac_auto_sync/setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+BASEDIR=$(dirname "$0")
+cd $BASEDIR
+
+type fswatch > /dev/null 2>&1 || brew install fswatch
+
+chmod +x auto_commit_push.sh
+
+cp com.eriksuen.autosync_dotfiles.plist ~/Library/LaunchAgents
+
+launchctl load -w ~/Library/LaunchAgents/com.eriksuen.autosync_dotfiles.plist
+
