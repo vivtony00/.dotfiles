babed19368cebf68547b2c881ced45bb56742b76
Erik Suen <suenfung2005@gmail.com>
2020-03-13 00:26:20 +0800

    auto commit

--- mac_auto_sync/auto_commit_push.sh
+++ mac_auto_sync/auto_commit_push.sh
@@ -3,6 +3,13 @@
 echo "[$(date)] Triggering Auto Sync Dotfiles"
 BASEDIR=$(dirname "$0")
 cd $BASEDIR
+
+# brew bundle dump
+cd brew_bundle
+brew bundle dump -f
+cd ..
+
+# git commit
 git add .
 git commit -m "auto commit" -a
 git push origin master
