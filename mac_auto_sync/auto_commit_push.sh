8ebdbabf7a3390010f45e3ea91b78cfc3c51bfc8
Erik Suen <suenfung2005@gmail.com>
2020-03-17 15:45:30 +0800

    auto commit

--- mac_auto_sync/auto_commit_push.sh
+++ mac_auto_sync/auto_commit_push.sh
@@ -6,6 +6,7 @@ cd $BASEDIR
 
 # brew bundle dump
 cd ../brew_bundle
+PATH=/Users/eriksuen/.pyenv/shims:/Users/eriksuen/bin:/usr/local/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/opt/fzf/bin
 echo $PATH
 echo $HOME
 /usr/local/bin/brew bundle dump -f --global
